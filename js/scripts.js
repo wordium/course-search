4192b74f5d0c3936108b9915ac81786597255ad1
Sandra Helsley <syh@ischool.berkeley.edu>
2014-04-13 11:51:14 -0700

    Should have all the form elements we need, now. More styling, some JS interaction.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
 $(document).ready(function () {
 
-  $('#advanced').hide();
+  var $form = $('form');
+  console.log($form);
+  var $text = $('#advancedState');
+  var $advanced = $('#advanced');
+  // uncomment when done testing:
+  //$advanced.hide();
 
+  // show/hide the advanced search stuff
   $('#toggleAdvSearch').on('click', function () {
-    $('#advanced').toggle();
     
-    if ($('#advancedState').text() === 'Show')
-      $('#advancedState').text('Hide');
+    // change the show/hide text in a fancy way
+    $text.parent().fadeOut(function() {
+      if ($text.text() === 'Show') {
+        $advanced.fadeIn(50);
+        $text.text('Hide');
+      }
+      else {
+        $advanced.fadeOut(50);
+        $text.text('Show');
+      }
+    }).fadeIn();
+
+    // less fancy way
+    /*
+    if ($text.text() === 'Show')
+      $text.text('Hide');
     else
-      $('#advancedState').text('Show');
+      $text.text('Show');
+    $('#advanced').toggle();
+    */
 
   });
+
 
   $('#time td').on('click', function() {
     var $td = $(this);
@@ -29,61 +51,67 @@ $(document).ready(function () {
 
 	testJSON();
 
-	function testJSON () {
-
-		var $test = $('#test');
-
-		$.getJSON('data/CourseStructure.json', function (data) {
-		  console.log('data: ' + data);
-
-      // course info
-		  $.each( data, function( key, course ) {
-
-        var BC = course.berkeleyConnect ? 'is' : 'is not',
-            AC = course.breadth.AC ? 'American Cultures' : '',
-            PS = course.breadth.PS ? 'Physical Sciences' : '',
-            RCA = course.breadth.RCA ? 'Reading & Composition A' : '';
-
-		    $test.append('<p> course: ' + key +
-		    	'<p>title: ' + course.title + 
-          '<p>type: ' + course.type +
-		    	'</p><p>department: ' + course.department + 
-		    	'</p><p>abbreviation: ' + course.deptAbbrev + ' ' + course.number + ', ' + course.credit + ' units</p>' +
-          '<p>crosslist: ' + course.crossListing + '</p>' +
-          '<p>desc: ' + course.description + '</p>' + 
-          '<p>format: ' + course.format + '</p>' + 
-          '<p>prereq: ' + course.prereq + ', coreq: ' + course.hasCoreqs + ', prereq for: ' + course.isPrereqFor + '</p>' +
-          '<p>restrictions: ' + course.restrictions + ', notes: ' + course.note + '</p>' +
-          '<p>offer history: ' + course.offerHist + '</p>' + 
-          '<p>courseThread: ' + course.courseThread + '</p>' +
-          '<p>fulfills these breadth requirements: ' + AC + ', ' + PS + ', ' + RCA + '</p>' + 
-          '<p>' + BC + ' Berkeley Connect</p>');
-
-        // class instance info
-		    var instance = [];
-		    instance = course.classInstance;
-        console.log(instance + ' length ' + instance.length);
-
-		    for (var i = 0; i < instance.length; i++) {
-		    	$test.append(
-            '<p>' + instance[i].semester + ', on ' + instance[i].days + ' at ' + instance[i].time + '</p>' + 
-            '<p>In ' + instance[i].location.room + ' ' + '<a href="">' + instance[i].location.building + '</a></p>' +
-            '<p>Taught by ' + instance[i].instructor + '.</p>' +
-            '<p>CCN: ' + instance[i].ccn + '</p>' +
-            '<p>Max seats: ' + instance[i].seats.max + ', enrolled: ' + instance[i].seats.enrolled + ', waitlist: ' +
-                               instance[i].seats.waitlist + ', available: ' + instance[i].seats.available + '</p>' +
-            '<p>Final: ' + instance[i].finalGroup + '</p>'
-          );
-		    }
-
-		    $test.append('<br />');
-				
-		  });
-
-		});
-	} // testJSON()
-
 });
+
+function submitForm () {
+  
+}
+
+
+
+function testJSON () {
+
+  var $test = $('#test');
+
+  $.getJSON('data/CourseStructure.json', function (data) {
+    console.log('data: ' + data);
+
+    // course info
+    $.each( data, function( key, course ) {
+
+      var BC = course.berkeleyConnect ? 'is' : 'is not',
+          AC = course.breadth.AC ? 'American Cultures' : '',
+          PS = course.breadth.PS ? 'Physical Sciences' : '',
+          RCA = course.breadth.RCA ? 'Reading & Composition A' : '';
+
+      $test.append('<p> course: ' + key +
+        '<p>title: ' + course.title + 
+        '<p>type: ' + course.type +
+        '</p><p>department: ' + course.department + 
+        '</p><p>abbreviation: ' + course.deptAbbrev + ' ' + course.number + ', ' + course.credit + ' units</p>' +
+        '<p>crosslist: ' + course.crossListing + '</p>' +
+        '<p>desc: ' + course.description + '</p>' + 
+        '<p>format: ' + course.format + '</p>' + 
+        '<p>prereq: ' + course.prereq + ', coreq: ' + course.hasCoreqs + ', prereq for: ' + course.isPrereqFor + '</p>' +
+        '<p>restrictions: ' + course.restrictions + ', notes: ' + course.note + '</p>' +
+        '<p>offer history: ' + course.offerHist + '</p>' + 
+        '<p>courseThread: ' + course.courseThread + '</p>' +
+        '<p>fulfills these breadth requirements: ' + AC + ', ' + PS + ', ' + RCA + '</p>' + 
+        '<p>' + BC + ' Berkeley Connect</p>');
+
+      // class instance info
+      var instance = [];
+      instance = course.classInstance;
+      console.log(instance + ' length ' + instance.length);
+
+      for (var i = 0; i < instance.length; i++) {
+        $test.append(
+          '<p>' + instance[i].semester + ', on ' + instance[i].days + ' at ' + instance[i].time + '</p>' + 
+          '<p>In ' + instance[i].location.room + ' ' + '<a href="">' + instance[i].location.building + '</a></p>' +
+          '<p>Taught by ' + instance[i].instructor + '.</p>' +
+          '<p>CCN: ' + instance[i].ccn + '</p>' +
+          '<p>Max seats: ' + instance[i].seats.max + ', enrolled: ' + instance[i].seats.enrolled + ', waitlist: ' +
+                             instance[i].seats.waitlist + ', available: ' + instance[i].seats.available + '</p>' +
+          '<p>Final: ' + instance[i].finalGroup + '</p>'
+        );
+      }
+
+      $test.append('<br />');
+      
+    });
+
+  });
+} // testJSON()
 
 
 
