ed403c3322d4a7f34f1239dd6bb995c4b1071198
Lisa Jervis <lj@ischool.berkeley.edu>
2014-04-18 20:31:57 -0700

    search 4! (and main file at point i abandonded it)

---
+++ data/search_results4.py
@@ -0,0 +1,60 @@
+#reads the courses.json file and outputs one file of results per search
+#search 1: fall 2014 classes in the public health major
+#search 2: all semesters L&S breadth reqs
+#search 3: all semesters keyword "sustainability"
+#search 4: fall 2014 MW before noon and anytime TuTh
+#search 5: fall 2014 open seats
+
+import json
+
+all_courses = json.load(open('courses.json'))
+
+courses_list = all_courses.items()
+#for course in courses_list, course is a tuple and course[0] is the courseID from the old file  and course[1] is the dict of actual info
+
+schedule_1stpass = [] #this will be the first pass to get the right courses apart from the others
+schedule_2ndpass = [] #this will be to remove the instances that don't fit the criteria
+schedule_3rdpass = []
+schedule_4thpass = []
+
+for course in courses_list:
+    del course[1]["pubHealthMaj"]
+
+    #handle search 4 part 1--this harvests courses with instances that meet the search criteria (though they also have instances that don't included in the JSON object)
+    #there are no classes that start in the a.m. and end after noon so just a.m. is fine for that too
+    for item in course[1]["classInstance"]:
+        if course not in schedule_1stpass:
+            if ((("M" in item["days"] or "W" in item["days"]) and "F" not in item["days"]) and ("a.m." in item["time"])) or ("Tu" in item["days"] or "Th" in item["days"]):
+                schedule_1stpass.append(course)
+
+
+#handle search 4 part 2--removing individual class instances from courses that have other instances that fit the criteria
+for course in schedule_1stpass:
+    for item in course[1]["classInstance"]:
+        if ("F" in item["days"]) or (("M" in item["days"] or "W" in item["days"]) and "a.m." not in item["time"]):
+            course[1]["classInstance"].remove(item)
+    schedule_2ndpass.append(course)
+    
+    
+for course in schedule_2ndpass:
+    for item in course[1]["classInstance"]:
+        if ("F" in item["days"]) or (("M" in item["days"] or "W" in item["days"]) and "a.m." not in item["time"]):
+            course[1]["classInstance"].remove(item)
+    schedule_3rdpass.append(course)
+
+file = open("s4_schedule_3rdpass.json", "w")
+
+for course in schedule_3rdpass:
+    for item in course[1]["classInstance"]:
+        if ("F" in item["days"]) or (("M" in item["days"] or "W" in item["days"]) and "a.m." not in item["time"]):
+            course[1]["classInstance"].remove(item)
+    schedule_4thpass.append(course)
+
+file = open("s4_schedule.json", "w")
+for course in schedule_4thpass:
+    file.write('\"' + course[0] + '\":')
+    file.write(json.dumps(course[1], sort_keys = True, indent = 4, ensure_ascii=False))
+    file.write(", \n")
+file.close()
+
+#still need to hand-delete MW 1:30-1 b/c no easy way to account for it boolean-ly and i had forgotten it was there
--- data/search_results_deprecated.py
+++ data/search_results_deprecated.py
@@ -12,8 +12,6 @@ all_courses = json.load(open('courses.json'))
 # print(type(all_courses["course38"]))
 
 courses_list = all_courses.items()
-print(type(courses_list))
-print(courses_list)
 #for course in courses_list, course is a tuple and course[0] is the courseID from the old file  and course[1] is the dict of actual info
 
 pub_health = []
@@ -32,26 +30,36 @@ for course in courses_list:
     
     #handle search 2
     if True in course[1]["breadth"].values():
+        del course[1]["pubHealthMaj"]
         breadth.append(course)
     
     #handle search 3
     if "sustainability" in course[1]["description"]:
+        del course[1]["pubHealthMaj"]
         sustain.append(course)
     
     #handle search 4 part 1--this harvests courses with instances that meet the search criteria (though they also have instances that don't included in the JSON object)
     #there are no classes that start in the a.m. and end after noon so just a.m. is fine for that too
-    for item in course[1]["classInstance"]:
-        if course not in schedule_1stpass:
-            if ("M" in item["days"] or "W" in item["days"] and "F" not in item["days"]) and "a.m." in item["time"]:
-                schedule_1stpass.append(course)
-            if ("Tu" in item["days"] or "Th" in item["days"]):
-                schedule_1stpass.append(course)
-
-print(schedule_1stpass)
-
-# for item in schedule_1stpass:
-#     print("ITEM")
-#     print(item)
+#     for item in course[1]["classInstance"]:
+#         if course not in schedule_1stpass:
+#             if ((("M" in item["days"] or "W" in item["days"]) and "F" not in item["days"]) and ("a.m." in item["time"])) or ("Tu" in item["days"] or "Th" in item["days"]):
+#                 del course[1]["pubHealthMaj"]
+#                 schedule_1stpass.append(course)
+# 
+# for course in schedule_1stpass:
+#     for item in course[1]["classInstance"]:
+#         if ("F" in item["days"]) or (("M" in item["days"] or "W" in item["days"]) and "a.m." not in item["time"]):
+#             print("REMOVING ITEM")
+#             print(item)
+#             course[1]["classInstance"].remove(item)
+#             print("PRTNING COURSE INSTANCES")
+#             print(course[1]["classInstance"])
+#             print("PRINTING FULL COURSE OBJECT")
+#             print(course)
+#     schedule_2ndpass.append(course)
+#     print(len(schedule_2ndpass))
+            
+            
 
 #handle search 4 part 2--this removes the non-matching instances from the overall course objects
 # for course in schedule_1stpass:
@@ -68,12 +76,12 @@ print(schedule_1stpass)
 #     file.write(", \n")
 # file.close()
 # 
-# file = open("s2_breadth.json", "w")
-# for course in breadth:
-#     file.write('\"' + course[0] + '\":')
-#     file.write(json.dumps(course[1], sort_keys = True, indent = 4, ensure_ascii=False))
-#     file.write(", \n")
-# file.close()
+file = open("s2_breadth.json", "w")
+for course in breadth:
+    file.write('\"' + course[0] + '\":')
+    file.write(json.dumps(course[1], sort_keys = True, indent = 4, ensure_ascii=False))
+    file.write(", \n")
+file.close()
 # 
 # file = open("s3_sustain.json", "w")
 # for course in sustain:
